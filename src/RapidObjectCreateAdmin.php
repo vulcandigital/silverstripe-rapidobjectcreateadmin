d6bac9f211ba7790acf9a237273d6c22a8fb98c9
Reece Alexander <reece.alexander@gmail.com>
2018-02-07 03:22:05 +1000

    Update RapidObjectCreateAdmin.php

--- src/RapidObjectCreateAdmin.php
+++ src/RapidObjectCreateAdmin.php
@@ -44,4 +44,65 @@ class RapidObjectCreateAdmin extends SingleObjectAdmin
 
         return $object;
     }
+    
+    /**
+     * @param array $data
+     * @param Form  $form
+     *
+     * @return mixed
+     */
+    public function doSave($data, $form)
+    {
+        $objectClass = static::config()->get('tree_class');
+
+        /** @var DataObject|Versioned $object */
+        $object = $objectClass::create();
+
+        $currentStage = Versioned::get_stage();
+        Versioned::set_stage('Stage');
+
+        $controller = Controller::curr();
+        if (!$object->canEdit()) {
+            return $controller->httpError(403);
+        }
+
+        try {
+            $form->saveInto($object);
+            $object->write();
+        } catch (ValidationException $e) {
+            $result = $e->getResult();
+            $form->loadMessagesFrom($result);
+
+            $responseNegotiator = new PjaxResponseNegotiator([
+                'CurrentForm' => function () use (&$form) {
+                    return $form->forTemplate();
+                },
+                'default'     => function () use (&$controller) {
+                    return $controller->redirectBack();
+                }
+            ]);
+            if ($controller->getRequest()->isAjax()) {
+                $controller->getRequest()->addHeader('X-Pjax', 'CurrentForm');
+            }
+            return $responseNegotiator->respond($controller->getRequest());
+        }
+
+        Versioned::set_stage($currentStage);
+        if ($objectClass::has_extension(Versioned::class)) {
+            if ($object->isPublished()) {
+                $this->publish($data, $form);
+            }
+        }
+
+        $link = '"' . $object->singular_name() . '"';
+        $message = _t('GridFieldDetailForm.Saved', 'Saved {name} {link}', [
+            'name' => $object->singular_name(),
+            'link' => $link
+        ]);
+
+        $form->sessionMessage($message, 'good');
+        $action = $this->edit(Controller::curr()->getRequest());
+
+        return $action;
+    }
 }
